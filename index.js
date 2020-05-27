21c226925fa7977f78149a76d41bcd81ca20e013
Huned Botee <24231+huned@users.noreply.github.com>
2020-05-27 09:05:53 -0700

    WIP sendgrid contacts export.

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 require('dotenv').config()
-const debug = require('debug')('index.js')
+const debug = require('debug')('sendgrid')
+const SendGrid = require('./lib/sendgrid')
 
-debug('this is some sample debugging output')
-process.stdout.write('put your code in ./index.js\n')
+const f = async () => {
+  const apiKey = process.env.SENDGRID_API_KEY
+  debug(`api key ${apiKey}`)
+  const sg = new SendGrid(apiKey)
+  const contacts = await sg.getContacts()
+  debug(`got contacts: ${contacts}`)
+}
+
+f()
